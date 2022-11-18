d0aba21bc7619ec6ce0c9b3713a5772efc05657e
thanhan671 <thanhannguyen67@gmail.com>
2022-11-18 08:33:13 +0700

    Add project files.

---
+++ VLUTUTORS/Areas/Admin/Controllers/ManageConsultingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageConsultingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
