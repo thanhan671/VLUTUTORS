0a6721cc98c4031db1f6a16ac3ec6d49ed1241a2
thanhan671 <thanhannguyen67@gmail.com>
2022-11-21 16:06:19 +0700

    fix and add db

---
+++ VLUTUTORS/Models/Context.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Models
+{
+    public class Context: DbContext
+    {
+        private string connectionString;
+
+        public Context(): base()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("appsettings.json", optional: false);
+
+            var configuration = builder.Build();
+
+            connectionString = configuration.GetConnectionString("MyConnection").ToString();
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
