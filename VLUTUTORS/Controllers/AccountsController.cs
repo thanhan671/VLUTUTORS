d1937cf60e2e2882bf40f0bb52b4bebb1ce0b932
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-11-21 23:40:17 +0700

    init login function
    on working of this task

--- VLUTUTORS/Controllers/AccountsController.cs
+++ VLUTUTORS/Controllers/AccountsController.cs
@@ -3,13 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using VLUTUTORS.Models;
 namespace VLUTUTORS.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Login()
+        private CP25Team01Context db = new CP25Team01Context();
+        private Func<bool, IActionResult> _loginSuccessCallback;
+
+        public IActionResult Login(Taikhoannguoidung taikhoannguoidung)
         {
+            string email = taikhoannguoidung.Email;
+            string password = taikhoannguoidung.MatKhau;
+
+            _loginSuccessCallback = LoginSuccessCall;
+            using(db)
+            {
+                var checkAccount = db.Taikhoannguoidungs.Where(acc => acc.Email.Equals(email.Trim())).FirstOrDefault();
+                if (checkAccount == null)
+                {
+                    return View();
+                }
+
+                if(password.Trim().Equals(checkAccount.MatKhau.Trim()))
+                {
+                    _loginSuccessCallback.Invoke(true);
+                }
+            }
+
             return View();
         }
 
@@ -17,5 +38,12 @@ namespace VLUTUTORS.Controllers
         {
             return View();
         }
+
+        private IActionResult LoginSuccessCall(bool status)
+        {
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+        
     }
 }
