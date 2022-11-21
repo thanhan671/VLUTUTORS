0cc9a0ea4fd0a14e38970c19d565fee28d432253
thanhan671 <thanhannguyen67@gmail.com>
2022-11-22 00:50:22 +0700

    update register

--- VLUTUTORS/Controllers/AccountsController.cs
+++ VLUTUTORS/Controllers/AccountsController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VLUTUTORS.Models;
 
 namespace VLUTUTORS.Controllers
 {
@@ -13,8 +15,23 @@ namespace VLUTUTORS.Controllers
             return View();
         }
 
-        public IActionResult Details()
+        [HttpPost]
+        public IActionResult Login(string HoTen, string Email, string MatKhau)
         {
+            string conString = @"Server=tuleap.vanlanguni.edu.vn,18082;Database=CP25Team01;User Id=CP25Team01; Password=Cap25t01;Integrated Security=True;Trusted_Connection=False;ApplicationIntent=ReadWrite;MultipleActiveResultSets=False";
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+                string query = "insert into Taikhoannguoidung(HoTen,Email,MatKhau) values('" + HoTen + "', '" + Email + "','" + MatKhau + "')";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                con.Open();
+                da.SelectCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch
+            {
+                con.Close();
+            }
             return View();
         }
     }
