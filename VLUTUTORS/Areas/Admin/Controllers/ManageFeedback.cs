72f95a0a35da7e038d2a8ee42bbeb7fb92a99260
thanhan671 <thanhannguyen67@gmail.com>
2022-12-02 17:07:49 +0700

    update manage feedback, consulting

--- VLUTUTORS/Areas/Admin/Controllers/ManageFeedback.cs
+++ VLUTUTORS/Areas/Admin/Controllers/ManageFeedback.cs
@@ -7,24 +7,14 @@ using System.Threading.Tasks;
 namespace VLUTUTORS.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class ManageTutors : Controller
+    public class ManageFeedback : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult ViewRegister()
-        {
-            return View();
-        }
-
-        public IActionResult DetailTutor()
-        {
-            return View();
-        }
-
-        public IActionResult UpdateTutor()
+        public IActionResult Detail()
         {
             return View();
         }
