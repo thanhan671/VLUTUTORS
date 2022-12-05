0f35a621958deb27eb74bf531f81b48b707a45d6
thanhan671 <thanhannguyen67@gmail.com>
2022-12-05 22:35:39 +0700

    small update

--- VLUTUTORS/Models/Taikhoannguoidung.cs
+++ VLUTUTORS/Models/Taikhoannguoidung.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 #nullable disable
@@ -12,15 +13,16 @@ namespace VLUTUTORS.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        public string HoTen { get; set; }
+        public string HoTen { get; set; } = null!;
 
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Không hợp lệ")]
-        public string Email { get; set; }
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email không đúng định dạng")]
+        [Remote(action: "Register", controller: "Accounts")]
+        public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "Vui lòng điền trường này!")]
         [MinLength(6, ErrorMessage = "Mật khẩu phải trên 6 ký tự")]
-        public string MatKhau { get; set; }
+        public string MatKhau { get; set; } = null!;
         public int? IdgioiTinh { get; set; }
         public string Sdt { get; set; }
         public DateTime? NgaySinh { get; set; }
