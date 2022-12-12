3af599c5ddfd2c3596ff448b6e93052d16d31003
thanhan671 <thanhannguyen67@gmail.com>
2022-12-12 23:39:45 +0700

    add some view

---
+++ VLUTUTORS/Areas/Admin/Controllers/ManageAccounts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageAccounts : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddNewAccounts()
+        {
+            return View();
+        }
+        public IActionResult EditAccounts()
+        {
+            return View();
+        }
+    }
+}
