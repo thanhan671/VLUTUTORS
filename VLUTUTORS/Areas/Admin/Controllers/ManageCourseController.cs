a8ae583e1baf2413a441796609751a942b7461cb
thanhan671 <thanhannguyen67@gmail.com>
2022-12-17 11:08:08 +0700

    add course and tutor subject

---
+++ VLUTUTORS/Areas/Admin/Controllers/ManageCourseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DetailCourse()
+        {
+            return View();
+        }
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+        public IActionResult EditCourse()
+        {
+            return View();
+        }
+        public IActionResult AddLesson()
+        {
+            return View();
+        }
+        public IActionResult EditLesson()
+        {
+            return View();
+        }
+    }
+}
---
+++ VLUTUTORS/Areas/Admin/Controllers/ManageTutorSubjectController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageTutorSubjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddSubject()
+        {
+            return View();
+        }
+        public IActionResult EditSubject()
+        {
+            return View();
+        }
+    }
+}
