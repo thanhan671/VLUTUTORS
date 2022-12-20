ab8578d70d858bfba26ac50d19eb40823d67ab2d
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-12-21 00:07:25 +0700

    update edit tutor profile

--- VLUTUTORS/Areas/Tutors/Controllers/AccountController.cs
+++ VLUTUTORS/Areas/Tutors/Controllers/AccountController.cs
@@ -1,15 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VLUTUTORS.Models;
+using VLUTUTORS.Support.Services;
 
 namespace VLUTUTORS.Areas.Tutors.Controllers
 {
     [Area("Tutors")]
     public class AccountController : Controller
     {
+        private readonly CP25Team01Context _db = DataManager.Instance().db();
+        [HttpGet]
         public IActionResult Index()
+        {
+            var userInfo = JsonConvert.DeserializeObject<Taikhoannguoidung>(HttpContext.Session.GetString("SessionInfo"));
+            Taikhoannguoidung taikhoannguoidung = _db.Taikhoannguoidungs.Find(userInfo.Id);
+
+            taikhoannguoidung.DepartmentItems = new SelectList(_db.Khoas, "Idkhoa", "TenKhoa", taikhoannguoidung.Idkhoa);
+            taikhoannguoidung.GenderItems = new SelectList(_db.Gioitinhs, "IdgioiTinh", "GioiTinh1", taikhoannguoidung.IdgioiTinh);
+            taikhoannguoidung.BankItems = new SelectList(_db.Nganhangs, "Id", "TenNganHangHoacViDienTu", taikhoannguoidung.IdnganHang);
+            taikhoannguoidung.Subject1Items = new SelectList(_db.Mongiasus, "IdmonGiaSu", "TenMonGiaSu", taikhoannguoidung.IdmonGiaSu1);
+            taikhoannguoidung.Subject2Items = new SelectList(_db.Mongiasus, "IdmonGiaSu", "TenMonGiaSu", taikhoannguoidung.IdmonGiaSu2);
+            //taikhoannguoidung.IdkhoaNavigation;
+            //Console.WriteLine("id khoa: " + taikhoannguoidung.IdkhoaNavigation.TenKhoa);
+
+            if (taikhoannguoidung == null)
+                return View();
+
+            return View(taikhoannguoidung);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string ss)
         {
             return View();
         }
