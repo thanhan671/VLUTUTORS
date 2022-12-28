4abf7dc9f707d902eda8df76c328319cda242c7e
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-12-28 23:00:39 +0700

    update for course

--- VLUTUTORS/Controllers/TutorTrainingController.cs
+++ VLUTUTORS/Controllers/TutorTrainingController.cs
@@ -10,10 +10,13 @@ namespace VLUTUTORS.Controllers
 {
     public class TutorTrainingController : Controller
     {
-        private CP25Team01Context db = new CP25Team01Context();
-        public async Task<IActionResult> IndexAsync()
+        private CP25Team01Context _db = new CP25Team01Context();
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string courseName)
         {
-            var noiDung = await db.Noidungs.FirstOrDefaultAsync(m => m.Id == 1);
+            var noiDung = await _db.Noidungs.FirstOrDefaultAsync(m => m.Id == 1);
+            
             ViewData["Slogan"] = noiDung.Slogan;
             ViewData["gtChanTrang"] = noiDung.GioiThieuChanTrang;
             ViewData["diaChi"] = noiDung.DiaChi;
@@ -21,13 +24,38 @@ namespace VLUTUTORS.Controllers
             ViewData["Email"] = noiDung.Email;
             ViewData["Fb"] = noiDung.Facebook;
             ViewData["gioiThieu"] = noiDung.GioiThieu;
-            ViewData["link"] = "https://www.youtube.com/embed/" + "EIwhQLQhlXE";
-            return View();
+            //ViewData["link"] = "https://www.youtube.com/embed/" + "EIwhQLQhlXE";
+
+            List<Baihoc> lesson = new List<Baihoc>();
+            Baihoc bai1 = new Baihoc(1, "how to marketing", "https://www.youtube.com/embed/" + "4ti_uK60nLk");
+            Baihoc bai2 = new Baihoc(2, "how to training", "https://www.youtube.com/embed/" + "Owf8emXuzAk");
+            Baihoc bai3 = new Baihoc(3, "how to make powerpoint", "https://www.youtube.com/embed/" + "XF34-Wu6qWU");
+            lesson.Add(bai1);
+            lesson.Add(bai2);
+            lesson.Add(bai3);
+            Baihoc baihoc = new Baihoc();
+            baihoc.courses = (from l in lesson
+                              select l.TenBaiHoc).ToList();
+
+            courseName = courseName == null ? bai1.TenBaiHoc : courseName;
+            Console.WriteLine("ten bai hoc la gi: " + courseName);
+            ViewData["link"] = lesson.Find(l => l.TenBaiHoc.Equals(courseName)).LinkBaiHoc;
+
+            return View(baihoc);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string courseButton, int id = 1)
+        {
+
+            Console.WriteLine("lesson: " + courseButton);
+            //int id = courseButton.
+            return RedirectToAction("Index", new { courseName = courseButton});
         }
 
         public async Task<IActionResult> DoTest()
         {
-            var noiDung = await db.Noidungs.FirstOrDefaultAsync(m => m.Id == 1);
+            var noiDung = await _db.Noidungs.FirstOrDefaultAsync(m => m.Id == 1);
             ViewData["Slogan"] = noiDung.Slogan;
             ViewData["gtChanTrang"] = noiDung.GioiThieuChanTrang;
             ViewData["diaChi"] = noiDung.DiaChi;
@@ -36,6 +64,11 @@ namespace VLUTUTORS.Controllers
             ViewData["Fb"] = noiDung.Facebook;
             ViewData["gioiThieu"] = noiDung.GioiThieu;
             return View();
+        }
+
+        private void LoadCourse()
+        {
+
         }
     }
 }
--- VLUTUTORS/Models/Baihoc.cs
+++ VLUTUTORS/Models/Baihoc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
@@ -13,5 +14,18 @@ namespace VLUTUTORS.Models
         public int IdKhoaHoc { get; set; }
 
         public virtual Khoadaotao IdKhoaHocNavigation { get; set; }
+
+        public Baihoc()
+        {
+        }
+        public Baihoc(int IdBaiHoc, string TenBaiHoc, string LinkBaiHoc)
+        {
+            this.IdBaiHoc = IdBaiHoc;
+            this.TenBaiHoc = TenBaiHoc;
+            this.LinkBaiHoc = LinkBaiHoc;
+        }
+
+        [NotMapped]
+        public List<string> courses { get; set; }
     }
 }
