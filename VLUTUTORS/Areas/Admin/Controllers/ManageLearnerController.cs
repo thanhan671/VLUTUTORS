0f5bb026f652a41a95564084edf4f9c7c11976d7
thanhan671 <thanhannguyen67@gmail.com>
2023-02-08 22:35:55 +0700

    add manage learner

---
+++ VLUTUTORS/Areas/Admin/Controllers/ManageLearnerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Quản trị viên người học")]
+    public class ManageLearnerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DetailLearner()
+        {
+            return View();
+        }
+    }
+}
