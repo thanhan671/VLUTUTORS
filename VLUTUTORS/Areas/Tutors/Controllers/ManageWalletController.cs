f31ebaf9e0e1f70e4e7d1d70e43d08d89f17c4c7
thanhan671 <thanhannguyen67@gmail.com>
2023-02-23 21:18:21 +0700

    update view

---
+++ VLUTUTORS/Areas/Tutors/Controllers/ManageWalletController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLUTUTORS.Areas.Tutors.Controllers
+{
+    public class ManageWalletController : Controller
+    {
+        [Area("Tutors")]
+
+        public IActionResult Index()
+        {
+            //string user = HttpContext.Session.GetString("LoginId");
+            //if (user == null)
+            //{
+            //    return RedirectToAction("Login", "Accounts", new { area = "default" });
+            //}
+            return View();
+        }
+    }
+}
