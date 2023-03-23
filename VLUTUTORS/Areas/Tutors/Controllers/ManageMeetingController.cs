226d7defdf6254e506867dfb41b613322913efbf
Thanh Tien Ngo <tienbap12@gmail.com>
2023-03-23 10:57:28 +0700

    update UI history booking of tutor

---
+++ VLUTUTORS/Areas/Tutors/Controllers/ManageMeetingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLUTUTORS.Areas.Tutors.Controllers
+
+{
+    [Area("Tutors")]
+    public class ManageMeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ManageHistoryBooking()
+        {
+            return View();
+        }
+    }
+}
