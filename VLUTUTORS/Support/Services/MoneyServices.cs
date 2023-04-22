a1e8d58f23ea44612bc770f30a17c30dd05ad549
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2023-04-22 22:42:07 +0700

    add function for money service

---
+++ VLUTUTORS/Support/Services/MoneyServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using VLUTUTORS.Models;
+
+namespace VLUTUTORS.Support.Services {
+    public class MoneyServices 
+    {
+        public static void AddMoney(int amount, int userId, CP25Team01Context _db, Action<bool> callback) 
+        {
+            //Taikhoannguoidung userAccount = _db.Taikhoannguoidungs.Where(a => a.Equals(userId)).FirstOrDefault();
+
+            //userAccount.Xu += amount;
+            //_db.Update(userAccount);
+            //_db.SaveChanges();
+
+            //callback?.Invoke(true);
+        }
+
+        public static void SubtractMoney(int amount, int userId, CP25Team01Context _db, Action<bool> callback) 
+        {
+            //Taikhoannguoidung userAccount = _db.Taikhoannguoidungs.Where(a => a.Equals(userId)).FirstOrDefault();
+            //if(userAccount.Xu < amount)
+            //{
+            //    callback?.Invoke(false);
+            //    return;
+            //}
+            //userAccount.Xu -= amount;
+            //_db.Update(userAccount);
+            //_db.SaveChanges();
+
+            //callback?.Invoke(true);
+        }
+    }
+}
